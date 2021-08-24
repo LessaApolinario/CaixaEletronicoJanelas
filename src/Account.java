90ecf72929206cb35d5a799113e50585ad119c56
Lessa Apolinario <lessaapolinario22@outlook.com>
2021-08-23 23:41:13 -0300

    Classe Account finalizada

--- src/Account.java
+++ src/Account.java
@@ -1,8 +1,11 @@
 public class Account {
+
   private String name;
   private String cpf;
-  double balance, withdraw;
+  private double balance;
+  private double withdraw;
 
+  // construtor
   public Account(String name, String cpf, double balance, double withdraw) {
     this.name = name;
 
@@ -15,6 +18,37 @@ public class Account {
     if (withdraw > 0.0) {
       this.withdraw = withdraw;
     }
+  }
 
+  // recuperando o nome do cliente
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  // recuperanndo o cpf do cliente
+  public void setCPF(String cpf) {
+    this.cpf = cpf;
+  }
+
+  public String getCPF() {
+    return cpf;
+  }
+
+  // realiza um despósito
+  public void deposit(double despositAmount) {
+    if (despositAmount > 0.0) {
+      balance += despositAmount;
+    }
+  }
+
+  // realiza um saque
+  public void withdraw(double withdrawAmount) {
+    if (withdrawAmount > 0.0) {
+      balance -= withdrawAmount;
+    }
   }
 }
